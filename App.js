b30803f7d43c5381c58bb641d703b8e14a01965b
Denzkristangacusan <gdenzkristan@gmail.com>
2024-09-13 11:06:46 +0800

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+const firstFavNum = 18;
+
+const secondFavNum = 12;
+
+const additionResult = firstFavNum + secondFavNum;
+
+const subtractionResult = firstFavNum - secondFavNum;
+
+const multiplicationResult = firstFavNum * secondFavNum;
+
+const divisionResult = firstFavNum / secondFavNum;
+
+const modulusResult = firstFavNum % secondFavNum;
+
+const exponentiationResult = firstFavNum ** secondFavNum;
+
+console.log(additionResult);
+console.log(subtractionResult);
+console.log(multiplicationResult);
+console.log(divisionResult);
+console.log(modulusResult);
+console.log(exponentiationResult);
